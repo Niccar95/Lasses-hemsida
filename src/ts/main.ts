07fe6505ccbac55a7f3d0e140f7c9725c6d0029f
Niccar95 <niccar.95@outlook.com>
2023-12-11 16:01:41 +0100

    aktivitets api tillagd

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,19 @@
+import axios from 'axios'
 import '../scss/style.scss'
+import { IBoredResponse } from './IBoredResponse';
 
+
+
+  const fetchData = async () : Promise<string> =>  {
+  
+      const response = await axios.get<IBoredResponse>("http://www.boredapi.com/api/activity?type=recreational");
+
+      console.log(response.data.activity);
+
+      return response.data.activity;   
+
+      
+  }
+ 
+fetchData();
+  
