180ad66a75798697fab0d98a05e270ac1242dc6d
Erduan Yousif <erduan.yousif@medieinstitutet.se>
2023-12-11 15:28:17 +0100

    skapade en div till bilderna

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,8 +6,10 @@ const response = await axios.get<IdogResponse>("https://dog.ceo/api/breeds/image
 const imageContainer = document.getElementById("imageContainer");
 response.data.message.forEach((dogImg => {
   console.log(dogImg)
+  const newContainer = document.createElement("div") as HTMLDivElement;
   const image = document.createElement("img");
   imageContainer?.appendChild(image);
+  imageContainer?.appendChild(newContainer);
   image.src = dogImg;
   image.alt = "Picture of dog";
   image.className = "dogImage";
