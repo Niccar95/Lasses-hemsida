cfaa26e64624e8f7b4ac4f14b81f2eb4ad1fe210
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-12-11 16:02:49 +0100

    Flyttade DOG FACT QUOTE

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,26 +3,6 @@ import axios from "axios";
 import "../scss/style.scss";
 import { IDogAPIdata } from "./Models/IDogAPIdata";
 
-//Hämta DOG API Facts
-const response = await axios.get<IDogAPIdata>(
-  "https://dogapi.dog/api/v2/facts?limit=10"
-);
-//Få fram fact
-const data = response.data.data[0].attributes.body;
-
-//Skapa Container för fakta quoten
-const factContainer = document.getElementById(
-  "dogFactContainer"
-) as HTMLDivElement;
-//Funktion för att skapa html
-const createHtmlForDogFact = (theData: string) => {
-  const factText = document.createElement("h3");
-  factText.innerHTML = theData;
-  factContainer.appendChild(factText);
-};
-//anropa funktionen med parameter för att få infon till API:et
-createHtmlForDogFact(data);
-
 const aboutLasse = document.getElementById("aboutLasse") as HTMLSelectElement;
 
 const aboutMeHeader = document.createElement("h3");
@@ -50,3 +30,25 @@ response.data.message.forEach((dogImg) => {
   image.alt = "Picture of dog";
   image.className = "dogImage";
 });
+
+//===================DOG FACT QUOTE==========================
+//Hämta DOG API Facts
+const response = await axios.get<IDogAPIdata>(
+  "https://dogapi.dog/api/v2/facts?limit=10"
+);
+//Få fram fact
+const data = response.data.data[0].attributes.body;
+
+//Skapa Container för fakta quoten
+const factContainer = document.getElementById(
+  "dogFactContainer"
+) as HTMLDivElement;
+//Funktion för att skapa html
+const createHtmlForDogFact = (theData: string) => {
+  const factText = document.createElement("h3");
+  factText.innerHTML = theData;
+  factContainer.appendChild(factText);
+};
+//anropa funktionen med parameter för att få infon till API:et
+createHtmlForDogFact(data);
+//===========================================================
