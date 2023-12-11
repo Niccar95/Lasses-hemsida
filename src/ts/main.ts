f2b907e2b6a8a1873bdcb258280452253323b257
Christian buaka <Christian.buaka@hotmail.com>
2023-12-11 14:21:24 +0100

    About Lasse

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,14 @@
 import '../scss/style.scss'
+
+
+const aboutLasse = document.getElementById('aboutLasse') as HTMLSelectElement;
+
+const aboutMeHeader = document.createElement('h3');
+aboutMeHeader.innerHTML = `Om mig`;
+const Ptag = document.createElement('p');
+
+Ptag.innerHTML = `Hej! Lasse heter jag och är (...) år. 
+Jag älskar hundar, mat och vin.`
+
+aboutLasse.appendChild(aboutMeHeader)
+aboutLasse.appendChild(Ptag)
