bf292b2301f876be9c4becd156e2b14a0eef96cb
Carolina Ekdal <Carolina.ekdal@gmail.com>
2023-12-11 15:53:53 +0100

    Created functioning API

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,23 @@
-import '../scss/style.scss'
+import axios from "axios";
+import "../scss/style.scss";
+import { IDogAPIdata } from "./Models/IDogAPIdata";
+
+//Hämta DOG API Facts
+const response = await axios.get<IDogAPIdata>(
+  "https://dogapi.dog/api/v2/facts?limit=10"
+);
+//Få fram fact
+const data = response.data.data[0].attributes.body;
+
+//Skapa Container för fakta quoten
+const factContainer = document.getElementById(
+  "dogFactContainer"
+) as HTMLDivElement;
+//Funktion för att skapa html
+const createHtmlForDogFact = (theData: string) => {
+  const factText = document.createElement("h3");
+  factText.innerHTML = theData;
+  factContainer.appendChild(factText);
+};
+//anropa funktionen med parameter för att få infon till API:et
+createHtmlForDogFact(data);
